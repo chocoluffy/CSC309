a41ac1e3fc6e2dd9ec9330fbb1bda42d28018604
HongjianHuang <huanghongjian@live.cn>
2016-04-06 19:27:20 -0400

    Added files via upload
    add users.js to the git, update the index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,1 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var ctrlMissions = require('../controllers/missions');
+var ctrlOthers = require('../controllers/others');
 
+// locations controller
+// router.get('/', ctrlMissions.missionlist);
+router.get('/', ctrlOthers.angularApp);
+router.get('/mission/:missionid', ctrlMissions.missioninfo);
+router.get('/mission/:missionid/review/new', ctrlMissions.addReview);
+router.post('/mission/:missionid/review/new', ctrlMissions.postAddReview);
+
+// others controller
+router.get('/others', ctrlOthers.about);
+
+
+module.exports = router;
